1b0543197c4eda177a48a1f3b990e4db533c4da9
Vaughan Kelly Guy <vkellyguy@gmail.com>
2015-09-01 14:19:19 -0400

    add app and app_delegate.rb

---
+++ app/app_delegate.rb
@@ -0,0 +1,7 @@
+class AppDelegate
+  def application(application, didFinishLaunchingWithOptions:launchOptions)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+
+    true
+  end
+end
